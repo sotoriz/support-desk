b73045e91373c8e1e0fa1d59dd3e6345af273c91
sotoriz <sotorisfx@gmail.com>
2026-04-06 18:25:59 +0100

    mPrepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -46,9 +46,13 @@ if(process.env.NODE_ENV === 'production'){
     // Set build folder as static
     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-    app.get('*', (req, res) => 
-    res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+    // app.get('*', (req, res) => 
+    // res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+    
 
+    app.get('*', (req, res) => 
+    res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
+    )
 }else{
     app.get('/', (req, res)=>{
         res.status(200).json({message: 'Welcome to Support Desk API'})

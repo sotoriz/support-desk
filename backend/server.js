1223342efa4148bdf65de654e7185f753741c944
sotoriz <sotorisfx@gmail.com>
2026-04-08 22:15:25 +0100

    update create build

--- backend/server.js
+++ backend/server.js
@@ -56,7 +56,6 @@ if(process.env.NODE_ENV === 'production'){
     // app.get('*', (req, res) => 
     // res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
     
-
     app.use((req, res) => {
     res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
 })
